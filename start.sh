9f9cbe04cc803564129e48471fbbabf4223c3d65
Saliha Haider <130080590+Saliha713@users.noreply.github.com>
2025-12-05 02:12:50 +0500

    Update Python commands to use python3

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@
 cd Backend
 
 # Install dependencies
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Start FastAPI server
-python -m uvicorn main:app --host 0.0.0.0 --port $PORT
+python3 -m uvicorn main:app --host 0.0.0.0 --port $PORT
